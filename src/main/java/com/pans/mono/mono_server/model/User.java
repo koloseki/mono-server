52e80b3f0ea7d0a4faa9be89fa2b578284fae48f
koloseki <koloseki@proton.me>
2026-03-21 14:59:07 +0100

    Added UserRepository interface for connection with database. Closes #2

---
+++ src/main/java/com/pans/mono/mono_server/model/User.java
@@ -0,0 +1,21 @@
+package com.pans.mono.mono_server.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "app_users")
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
---
+++ src/main/java/com/pans/mono/mono_server/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.pans.mono.mono_server.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.pans.mono.mono_server.model.User;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+}
